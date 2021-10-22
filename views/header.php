3f6c74e20d0a5fe6a339722cb7122656475d26e8
Pacojc <pacojc1999@gmail.com>
2021-10-22 14:06:59 +0200

    bootstrap menu lista

--- views/header.php
+++ views/header.php
@@ -7,9 +7,13 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-    <a href="index.php?controller=ResourcesController&action=list">Recursos</a>
-    <a href="index.php?controller=TimeSlotsController&action=list">TimeSlots</a>
-    <a href="index.php?controller=usersController&action=list">Users</a>
+<nav  class="navbar navbar-expand-md navbar-dark bg-dark mb-4" role="navigation">
+  <!-- Navbar content -->
+
+    <a  class="navbar-brand" href="index.php?controller=ResourcesController&action=list">Recursos</a>
+    <a  class="navbar-brand" href="index.php?controller=TimeSlotsController&action=list">TimeSlots</a>
+    <a  class="navbar-brand" href="index.php?controller=usersController&action=list">Users</a>
+</nav>
     <div class="container">
         
     
--- views/menuList.php
+++ views/menuList.php
@@ -1,13 +1,9 @@
 <?php
-echo "<h4> Bienvenido usuario </h4><br>
-<h4> Menu de opciones </h4> <br>
-<ul>  
-    <li><a href='index.php?controller=ResourcesController&action=list'>Mantenimiento de Recursos</a></li>
-    <li><a href='index.php?controller=TimeSlotsController&action=list'>Mantenimiento de TimeSlots</a> </li>
-    <li><a href='index.php?controller=usersController&action=list'>Mantenimiento de Users</a> </li>
-
-</ul>
-
-
-
-";
+echo "<br><h6> Bienvenido usuario </h6><br>
+<h6> Menu de opciones </h6> <br>
+<ul class='list-group-flush '> 
+    <li class='list-group-item '><a href='index.php?controller=ResourcesController&action=list'>Mantenimiento de Recursos</a></li>
+    <li class='list-group-item '><a href='index.php?controller=TimeSlotsController&action=list'>Mantenimiento de TimeSlots</a> </li>
+    <li class='list-group-item '><a href='index.php?controller=usersController&action=list'>Mantenimiento de Users</a> </li>
+<p></p>
+</ul>";
